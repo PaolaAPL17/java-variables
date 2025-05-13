5871e2ea7425483cf51269d885e3565c37a68d62
paolap <paopin2024@gmail.com>
2025-05-13 23:20:06 +0200

    ejercicio3

--- AboutMe.java
+++ AboutMe.java
@@ -3,14 +3,22 @@ public class AboutMe {
         //inicializar una variable (del tipo que veas conveniente) para cada uno de los siguentes datos: nombre, edad, país, lenguaje de programación, horas, pasatiempo, lugar y reemplaza en el texto de impresión, la variable que corresponda.
 
         //Escribe tu código aquí
+        String name = "Paola";
+        int age = 30;
+        String country = "Colombia";
+        String programingLenguage = "Java";
+        int hours = 5;
+        String[] hobbies = {"Paint", "Dance", "Read"};
+        String place = "Beach";
+
 
         //Reemplaza <name> y <age> concatenando el String con las variables creadas. 
-        System.out.println("Mi nombre es <name>, tengo <age> años y soy de <country>.");
+        System.out.println("Mi nombre es " + name + ", tengo " + age + " años y soy de " + country + ".");
 
         //Reemplaza <programmingLanguage> y <hours> concatenando el String con las variables creadas. 
-        System.out.println("Estoy estudiando <programmingLanguage> y practico <hours> horas al día.");
+        System.out.println("Estoy estudiando " + programingLenguage + " y practico " + hours + " horas al día.");
 
         //Reemplaza <hobby> y <place> concatenando el String con las variables creadas.
-        System.out.println("Mi pasatiempo favorito es <hobby> y lo practico en <place>");
+        System.out.println("Mi pasatiempo favorito es " + hobbies[3] + " y lo practico en " + place);
     }
 }
